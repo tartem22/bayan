edc714eaf620bb0ef398a08c1adbcd273e3b670d
tartem22 <103363061+tartem22@users.noreply.github.com>
2022-10-22 23:27:40 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -38,12 +38,12 @@ TEST(duplicate_finder, test_src32)
 
     std::vector<std::vector<std::string>> expected;
     expected.push_back(std::vector<std::string>());
-    expected.at(0).push_back("test_data/test_data_1/test_data_11/test_data_111/test1.txt");
-    expected.at(0).push_back("test_data/test_data_1/test_data_11/test1.txt");
-    expected.at(0).push_back("test_data/test_data_1/test1.txt");
+    expected.at(0).push_back("test_data/test_data_1/test_data_12/test0.txt");
+    expected.at(0).push_back("test_data/test0.txt");
     expected.push_back(std::vector<std::string>());
-    expected.at(1).push_back("test_data/test_data_1/test_data_12/test0.txt");
-    expected.at(1).push_back("test_data/test0.txt");
+    expected.at(1).push_back("test_data/test_data_1/test_data_11/test_data_111/test1.txt");
+    expected.at(1).push_back("test_data/test_data_1/test_data_11/test1.txt");
+    expected.at(1).push_back("test_data/test_data_1/test1.txt");
 
     EXPECT_EQ(duplicates, expected);
 }
@@ -83,12 +83,12 @@ TEST(duplicate_finder, test_md5)
 
     std::vector<std::vector<std::string>> expected;
     expected.push_back(std::vector<std::string>());
-    expected.at(0).push_back("test_data/test_data_1/test_data_11/test_data_111/test1.txt");
-    expected.at(0).push_back("test_data/test_data_1/test_data_11/test1.txt");
-    expected.at(0).push_back("test_data/test_data_1/test1.txt");
+    expected.at(0).push_back("test_data/test_data_1/test_data_12/test0.txt");
+    expected.at(0).push_back("test_data/test0.txt");
     expected.push_back(std::vector<std::string>());
-    expected.at(1).push_back("test_data/test_data_1/test_data_12/test0.txt");
-    expected.at(1).push_back("test_data/test0.txt");
+    expected.at(1).push_back("test_data/test_data_1/test_data_11/test_data_111/test1.txt");
+    expected.at(1).push_back("test_data/test_data_1/test_data_11/test1.txt");
+    expected.at(1).push_back("test_data/test_data_1/test1.txt");
 
     EXPECT_EQ(duplicates, expected);
 }
